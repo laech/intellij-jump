dbafea74d82145a79fdec9b44fbe90fd6ebf7979
Lae <llaechen@gmail.com>
2019-06-30 23:05:43 +1200

    Skip fold regions.

--- src/main/java/com/gitlab/lae/intellij/jump/editor/Editors.java
+++ src/main/java/com/gitlab/lae/intellij/jump/editor/Editors.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.KeyboardShortcut;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.FoldRegion;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,8 +13,12 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static com.intellij.openapi.actionSystem.IdeActions.ACTION_EDITOR_ESCAPE;
+import static java.util.Arrays.stream;
 import static java.util.Collections.emptySet;
+import static java.util.Objects.requireNonNull;
 import static java.util.Spliterators.spliteratorUnknownSize;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
 import static java.util.stream.Collectors.toSet;
 import static java.util.stream.StreamSupport.intStream;
 import static kotlin.text.StringsKt.indexOf;
@@ -37,7 +42,6 @@ public final class Editors {
             String query,
             boolean ignoreCase
     ) {
-        // TODO take folding into account
         Rectangle area = editor.getScrollingModel().getVisibleArea();
         int start = getAreaStartOffset(editor, area);
         int end = getAreaEndOffset(editor, area);
@@ -51,41 +55,83 @@ public final class Editors {
             int start,
             int end
     ) {
-        PrimitiveIterator.OfInt it = new PrimitiveIterator.OfInt() {
+        return intStream(
+                spliteratorUnknownSize(
+                        new OffsetIterator(editor, query, ignoreCase, start, end),
+                        0),
+                false);
+    }
 
-            int currentStart = start - 1;
-            CharSequence chars = editor.getDocument()
+    private static class OffsetIterator implements PrimitiveIterator.OfInt {
+
+        private final NavigableMap<Integer, FoldRegion> regionsByStartOffset;
+        private final CharSequence chars;
+        private final String query;
+        private final boolean ignoreCase;
+
+        private int currentStart;
+        private int next = -1;
+        private boolean done = false;
+
+        OffsetIterator(
+                Editor editor,
+                String query,
+                boolean ignoreCase,
+                int start,
+                int end
+        ) {
+            this.currentStart = start - 1;
+            this.chars = editor.getDocument()
                     .getImmutableCharSequence()
                     .subSequence(0, end);
+            this.regionsByStartOffset = getFoldRegionByStartOffset(editor);
+            this.query = requireNonNull(query);
+            this.ignoreCase = ignoreCase;
+        }
 
-            int next = -1;
-            boolean done = false;
+        @Override
+        public boolean hasNext() {
+            if (next < 0) computeNext();
+            return next >= 0;
+        }
 
-            @Override
-            public boolean hasNext() {
-                if (next < 0) computeNext();
-                return next >= 0;
-            }
+        @Override
+        public int nextInt() {
+            if (next < 0) computeNext();
+            if (next < 0) throw new NoSuchElementException();
+            int result = next;
+            next = -1;
+            return result;
+        }
 
-            @Override
-            public int nextInt() {
-                if (next < 0) computeNext();
-                if (next < 0) throw new NoSuchElementException();
-                int result = next;
-                next = -1;
-                return result;
-            }
-
-            private void computeNext() {
-                if (done) return;
+        private void computeNext() {
+            boolean found = false;
+            while (!done && !found) {
                 currentStart = indexOf(chars, query, currentStart + 1, ignoreCase);
                 next = currentStart;
                 if (next < 0) {
                     done = true;
+                } else {
+
+                    Map.Entry<Integer, FoldRegion> entry =
+                            regionsByStartOffset.floorEntry(next);
+
+                    if (entry == null || entry.getValue().getEndOffset() <= next) {
+                        found = true;
+                    }
                 }
             }
-        };
-        return intStream(spliteratorUnknownSize(it, 0), false);
+        }
+    }
+
+    private static NavigableMap<Integer, FoldRegion> getFoldRegionByStartOffset(Editor editor) {
+        return stream(editor.getFoldingModel().getAllFoldRegions())
+                .filter(o -> !o.isExpanded())
+                .collect(toMap(
+                        FoldRegion::getStartOffset,
+                        identity(),
+                        (a, b) -> a.getEndOffset() >= b.getEndOffset() ? a : b,
+                        TreeMap::new));
     }
 
     private static int getAreaStartOffset(Editor editor, Rectangle area) {
@@ -103,7 +149,7 @@ public final class Editors {
         if (action == null) {
             return emptySet();
         }
-        return Arrays.stream(action.getShortcutSet().getShortcuts())
+        return stream(action.getShortcutSet().getShortcuts())
                 .map(it -> it instanceof KeyboardShortcut &&
                         ((KeyboardShortcut) it).getSecondKeyStroke() == null ?
                         ((KeyboardShortcut) it).getFirstKeyStroke() : null)
--- src/test/java/com/gitlab/lae/intellij/jump/editor/EditorsTest.java
+++ src/test/java/com/gitlab/lae/intellij/jump/editor/EditorsTest.java
@@ -1,6 +1,8 @@
 package com.gitlab.lae.intellij.jump.editor;
 
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.FoldRegion;
+import com.intellij.openapi.editor.FoldingModel;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
 
 import static com.gitlab.lae.intellij.jump.editor.Editors.searchOffsets;
@@ -12,9 +14,30 @@ public final class EditorsTest extends LightPlatformCodeInsightFixtureTestCase {
         myFixture.configureByText(PLAIN_TEXT, "HelloWorld");
         Editor editor = myFixture.getEditor();
         assertOrderedEquals(
-                new int[]{4, 6},
-                searchOffsets(editor, "o", true, 0, 10).toArray());
-
+                searchOffsets(editor, "o", true, 0, 10).toArray(),
+                new int[]{4, 6});
     }
 
+    public void testSearchOffsetsSkipFoldRegions() {
+        myFixture.configureByText(
+                PLAIN_TEXT, "" +
+                        "HelloWorld\n" +
+                        "HelloWorld\n" +
+                        "HelloWorld\n" +
+                        "HelloWorld");
+
+        Editor editor = myFixture.getEditor();
+        FoldingModel foldingModel = editor.getFoldingModel();
+        foldingModel.runBatchFoldingOperation(() -> {
+            FoldRegion r1 = foldingModel.addFoldRegion(0, 20, "...");
+            FoldRegion r2 = foldingModel.addFoldRegion(30, 40, "...");
+            assertNotNull(r1);
+            assertNotNull(r2);
+            r1.setExpanded(false);
+            r2.setExpanded(false);
+        });
+        assertOrderedEquals(
+                searchOffsets(editor, "o", true, 0, 40).toArray(),
+                new int[]{26, 28});
+    }
 }
