0aa0f2ab0f4dcd31f878950a7e1aa295389bd933
Lae Chen <laec@proton.me>
2023-06-19 20:36:40 +1200

    Update build tools.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.jetbrains.kotlin.jvm").version("1.5.0")
-  id("org.jetbrains.intellij").version("0.7.3")
+  id("org.jetbrains.kotlin.jvm").version("1.8.22")
+  id("org.jetbrains.intellij").version("1.14.1")
   id("com.diffplug.spotless").version("6.19.0")
 }
 
@@ -13,28 +12,16 @@ version = "0.2.3-SNAPSHOT"
 
 repositories { mavenCentral() }
 
-tasks.withType<KotlinCompile> {
-  kotlinOptions.jvmTarget = "11"
-  kotlinOptions.jdkHome =
-      javaToolchains
-          .compilerFor { languageVersion.set(JavaLanguageVersion.of(11)) }
-          .get()
-          .metadata
-          .installationPath
-          .asFile
-          .absolutePath
-}
+kotlin { jvmToolchain { languageVersion.set(JavaLanguageVersion.of(11)) } }
 
-tasks.withType<Test> { testLogging { exceptionFormat = TestExceptionFormat.FULL } }
-
-intellij {
-  updateSinceUntilBuild = false
-  version = "2021.1"
-}
-
-dependencies { implementation(kotlin("stdlib-jdk8")) }
+intellij { version.set("2021.1") }
 
 spotless {
   kotlin { ktfmt() }
   kotlinGradle { ktfmt() }
+}
+
+tasks {
+  test { testLogging { exceptionFormat = TestExceptionFormat.FULL } }
+  patchPluginXml { untilBuild.set("") }
 }
